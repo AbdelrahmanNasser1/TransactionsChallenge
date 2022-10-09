3a72a57d62619f7dcc6b81f27bc678ff9198a4af
abdelrahman nasser <nabdo5021@gmail.com>
2022-10-10 00:08:43 +0200

    Updating my docker-compose file

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"FirstWeek/Internal/Services"
 	"FirstWeek/Internal/adapter/api"
 	"FirstWeek/Internal/adapter/db"
+	"FirstWeek/Internal/adapter/stream"
 	"fmt"
 )
 
@@ -16,13 +17,14 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to read Config file")
 	}
-
+	stream.KafkaConsumer(config)
 	con, err := db.NewDBConnection(config)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-
 	tranRepo := Repository.NewDefaultRepository(con)
 	tranSer := Services.NewDefaultService(tranRepo)
 	api.NewTransactionController(tranSer, config)
+	//stream.KafkaConsumer(config)
+
 }
